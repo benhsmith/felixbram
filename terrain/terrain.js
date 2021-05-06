b10869f057314b942d9e76778c801a9f398e97e4
Ben Smith <bsmith@carbonblack.com>
2021-05-05 18:33:58 -0700

    Added drawing of ground

--- terrain/terrain.js
+++ terrain/terrain.js
@@ -7,7 +7,7 @@ function renderTree(x, y) {
 
   ctx.strokeStyle = "brown";
   ctx.moveTo(x, y);
-  ctx.lineTo(x, y + 10);
+  ctx.lineTo(x, y - 10);
   ctx.stroke();
 
   ctx.closePath();
@@ -15,16 +15,32 @@ function renderTree(x, y) {
   // treetop
   ctx.beginPath();
 
-  ctx.arc(x, y - 10, 10, 0, Math.PI * 2, false);
+  ctx.arc(x, y - 20, 10, 0, Math.PI * 2, false);
   ctx.fillStyle = "green";
   ctx.fill();
 
   ctx.closePath();
 }
 
+function renderGround(x, y) {
+  ctx.beginPath();
+
+  ctx.strokeStyle = "brown";
+  ctx.moveTo(x, canvas.height);
+  ctx.lineTo(x, y);
+  ctx.stroke();
+
+  ctx.closePath();
+
+}
+
 function renderObject(type, x, y) {
 
   ctx.beginPath();
+
+  if (type === "ground") {
+    renderGround(x, y);
+  }
 
   if (type === "tree") {
     renderTree(x, y);
@@ -32,9 +48,18 @@ function renderObject(type, x, y) {
 
 }
 
+function randomNumber(max) {
+  return Math.round(Math.random() * max);
+}
+
 var xpos = 5;
 var ypos = canvas.height / 2;
+var lastTree = 0;
 
-for (;xpos < canvas.width;xpos += 15) {
-	renderObject("tree", xpos, ypos);
+for (;xpos < canvas.width;xpos += 1) {
+    renderObject("ground", xpos, ypos);
+    if (xpos - lastTree > 15) {
+      renderObject("tree", xpos, ypos);
+      lastTree = xpos;
+    }
 }
