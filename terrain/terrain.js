6077b95f7e62efd686f2e619e817a9489b55749a
Ben Smith <bsmith@carbonblack.com>
2021-06-03 10:54:52 -0700

    Adds grass to landscape Adds scrolling with right/left keys

--- terrain/terrain.js
+++ terrain/terrain.js
@@ -25,13 +25,21 @@ function renderTree(x, y) {
 function renderGround(x, y) {
   ctx.beginPath();
 
-  ctx.strokeStyle = "#622b10";
+  ctx.strokeStyle = "#00ba15";
   ctx.moveTo(x, canvas.height);
   ctx.lineTo(x, y);
   ctx.stroke();
 
   ctx.closePath();
 
+  ctx.beginPath();
+
+  ctx.strokeStyle = "brown";
+  ctx.moveTo(x, canvas.height);
+  ctx.lineTo(x, y + 10);
+  ctx.stroke();
+
+  ctx.closePath();
 }
 
 function renderObject(type, x, y) {
@@ -46,6 +54,10 @@ function renderObject(type, x, y) {
     renderTree(x, y);
   }
 
+  if (type === "cactus") {
+    renderCactus(x, y);
+  }
+
 }
 
 function randomNumber(min, max) {
@@ -53,21 +65,69 @@ function randomNumber(min, max) {
   return max - Math.round(Math.random() * diff);
 }
 
+var world_data = []
 
-var xpos = 5;
-var ypos = canvas.height / 2;
-var lastTree = 0;
+var height = canvas.height / 2;
+var last_tree = 0;
 
-// this doesn't do anything yet
-var heights = []
+function addMoreLand(index) {
+  var change = randomNumber(-5, 5);
+  height += change;
 
-for (; xpos < canvas.width; xpos += 1) {
-  height = randomNumber(-10, 10);
-  heights.push(height)
-  ypos += height
-  renderObject("ground", xpos, ypos);
-  if (xpos - lastTree > randomNumber(5, 100)) {
-    renderObject("tree", xpos, ypos);
-    lastTree = xpos;
+  var has_tree = false;
+  if (index - last_tree > randomNumber(5, 100)) {
+    last_tree = index;
+    has_tree = true;
+  }
+
+  world_data.push([height, has_tree]);
+}
+
+var world_pos = 0;
+
+function renderLandscape() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  var status = document.getElementById("status");
+  status.textContent = world_pos;
+
+  for (xpos = 0; xpos < canvas.width; xpos += 1) {
+    if (xpos + world_pos >= world_data.length) {
+      addMoreLand(xpos + world_pos);
+    }
+    var data = world_data[xpos + world_pos];
+    var ypos = data[0];
+    var tree = data[1];
+    renderObject("ground", xpos, ypos);
+    if (tree) {
+      renderObject("tree", xpos, ypos);
+    }
   }
 }
+
+
+function keyDownHandler(e) {
+  if(e.key == "Right" || e.key == "ArrowRight") {
+    world_pos += 1;
+
+    renderLandscape();
+  }
+  else if(e.key == "Left" || e.key == "ArrowLeft") {
+    if (world_pos > 0) {
+      world_pos -= 1;
+    }
+    renderLandscape();
+  }
+  /*
+  else if(e.key == "Down" || e.key == "ArrowDown") {
+    y = y + speed;
+  }
+  else if(e.key == "Up" || e.key == "ArrowUp") {
+    y = y - speed;
+  }
+  */
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+
+renderLandscape();
